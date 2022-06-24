38064b22c08299c2e02c5afa24512189119d5119
Mubaraq Yaqoub-Okponobi <oshiole94@gmail.com>
2022-06-24 21:50:16 +0100

    PR

--- app/src/main/java/com/codose/chats/views/cashForWork/CashForWorkFragment.kt
+++ app/src/main/java/com/codose/chats/views/cashForWork/CashForWorkFragment.kt
@@ -26,7 +26,7 @@ class CashForWorkFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        cashForWorkViewModel.getCashForWorks(PrefUtils.getNGOId().toString())
+//        cashForWorkViewModel.getCashForWorks(PrefUtils.getNGOId().toString())
     }
 
     override fun onCreateView(
@@ -39,6 +39,8 @@ class CashForWorkFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        cashForWorkViewModel.getCashForWorks(PrefUtils.getNGOId().toString())
 
         adapter = CashForWorkAdapter(CashForWorkClickListener {
             findNavController().navigate(CashForWorkFragmentDirections.actionCashForWorkFragmentToCashForWorkTaskFragment(
