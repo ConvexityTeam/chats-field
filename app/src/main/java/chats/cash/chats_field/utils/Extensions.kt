d522a6ef294611cec2bc332813a726048580ec9b
Segun Francis <francis4dblues@gmail.com>
2022-10-17 23:31:20 +0100

    Privacy Policy and Terms of Use implementation

--- app/src/main/java/chats/cash/chats_field/utils/Extensions.kt
+++ app/src/main/java/chats/cash/chats_field/utils/Extensions.kt
@@ -2,8 +2,13 @@ package chats.cash.chats_field.utils
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Color
+import android.text.*
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.util.Patterns
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import chats.cash.chats_field.utils.ChatsFieldConstants.COMPLETE
@@ -196,4 +201,35 @@ fun Boolean?.toStatusString(): String {
     } else {
         INCOMPLETE
     }
+}
+
+fun TextView.makeLinks(vararg links: Pair<String, View.OnClickListener>) {
+    val spannableString = SpannableString(this.text)
+    var startIndexOfLink = -1
+    for (link in links) {
+        val clickableSpan = object : ClickableSpan() {
+            override fun updateDrawState(textPaint: TextPaint) {
+                // use this to change the link color
+                textPaint.color = Color.rgb(0, 40, 85)
+                // toggle below value to enable/disable
+                // the underline shown below the clickable text
+                textPaint.isUnderlineText = true
+            }
+
+            override fun onClick(view: View) {
+                Selection.setSelection((view as TextView).text as Spannable, 0)
+                view.invalidate()
+                link.second.onClick(view)
+            }
+        }
+        startIndexOfLink = this.text.toString().indexOf(link.first, startIndexOfLink + 1)
+//      if(startIndexOfLink == -1) continue // todo if you want to verify your texts contains links text
+        spannableString.setSpan(
+            clickableSpan, startIndexOfLink, startIndexOfLink + link.first.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+    }
+    this.movementMethod =
+        LinkMovementMethod.getInstance() // without LinkMovementMethod, link can not click
+    this.setText(spannableString, TextView.BufferType.SPANNABLE)
 }
