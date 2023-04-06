744e10463204cec9b337f2f7ca4e54d01c412f26
Tule Simon <58936865+TuleSimon@users.noreply.github.com>
2023-04-06 13:24:43 +0100

    started localization5

--- app/src/test/java/chats/cash/chats_field/network/api/ConvexityApiTest.kt
+++ app/src/test/java/chats/cash/chats_field/network/api/ConvexityApiTest.kt
@@ -1,5 +1,6 @@
 package chats.cash.chats_field.network.api
 
+import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
 import org.junit.runner.RunWith
@@ -8,7 +9,7 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class ConvexityApiTest {
 
-    lateinit var mockWebServer:Mock
+    lateinit var mockWebServer:MockWebServer
     lateinit var convexityApiService: ConvexityApiService
 
     @Before
